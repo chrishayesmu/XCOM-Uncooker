69810e554c296d4e8088814dcc6d75b71872e594
Chris Hayes <chrishayesmu@gmail.com>
2024-07-06 22:16:32 -0700

    Materials: attempt to hook up texture parameters to empty outputs automatically

--- XCOM-Uncooker/Unreal/Physical/ObjectSubtypes/Materials/UMaterial.cs
+++ XCOM-Uncooker/Unreal/Physical/ObjectSubtypes/Materials/UMaterial.cs
@@ -62,5 +62,101 @@ namespace XCOM_Uncooker.Unreal.Physical.ObjectSubtypes.Materials
                 expressionsProp.NumElements = expressionsProp.Data.Length;
             }
         }
+
+        public override void PostArchiveCloneComplete()
+        {
+            // When the same input is used multiple times, we're just using the first one that matches
+            TryPopulateInputIfNeeded("DiffuseColor", "Diffuse");
+            TryPopulateInputIfNeeded("DiffuseColor", "Diffuse_Base");
+            TryPopulateInputIfNeeded("DiffuseColor", "Diffuse Map");
+            TryPopulateInputIfNeeded("DiffuseColor", "Damage_Diffuse");
+            TryPopulateInputIfNeeded("DiffuseColor", "Diffuse_Damage");
+            TryPopulateInputIfNeeded("SpecularPower", "SpcColor_SpcGloss");
+            TryPopulateInputIfNeeded("OpacityMask", "Spc_Ems_Mpc_Opc");
+            TryPopulateInputIfNeeded("OpacityMask", "Spc_Ems_Ref_Opc");
+            TryPopulateInputIfNeeded("Normal", "Normal");
+            TryPopulateInputIfNeeded("Normal", "Normal Map");
+            TryPopulateInputIfNeeded("Normal", "Damage_Normal");
+        }
+
+        /// <summary>
+        /// Attempts to fill in one of the main material inputs using a parameter expression.
+        /// The input must be unpopulated, and the parameter must exist on this material.
+        /// </summary>
+        /// <param name="inputName"></param>
+        /// <param name="parameterName"></param>
+        private void TryPopulateInputIfNeeded(string inputName, string parameterName)
+        {
+            var inputProp = GetSerializedProperty(inputName) as USerializedStructProperty;
+
+            // If there was originally an input but it got stripped during cooking, then there
+            // will be a property but its Expression will be null. If the property itself isn't there,
+            // then this property was never set in the first place.
+            if (inputProp == null)
+            {
+                return;
+            }
+
+            var inputExpression = inputProp.GetSerializedProperty("Expression") as USerializedObjectProperty;
+
+            if (inputExpression != null && inputExpression.ObjectIndex != 0)
+            {
+                // TODO: even with an expression set, it might need replacing (e.g. a switch param that has no inputs anymore)
+                return;
+            }
+
+            // Now look for an expression with the matching parameter name
+            var expressionsProp = GetSerializedProperty("Expressions") as USerializedArrayProperty;
+
+            if (expressionsProp == null)
+            {
+                return;
+            }
+
+            int matchingParameterExpressionId = 0;
+
+            for (int i = 0; i < expressionsProp.NumElements; i++)
+            {
+                var objProp = expressionsProp.Data[i] as USerializedObjectProperty;
+
+                if (objProp.ObjectIndex != 0)
+                {
+                    var expressionObj = Archive.GetObjectByIndex(objProp.ObjectIndex);
+
+                    if (expressionObj == null)
+                    {
+                        continue;
+                    }
+
+                    var parameterNameProp = expressionObj.GetSerializedProperty("ParameterName") as USerializedNameProperty;
+
+                    if (parameterNameProp != null && parameterNameProp.Value == parameterName)
+                    {
+                        matchingParameterExpressionId = objProp.ObjectIndex;
+                        break;
+                    }
+                }
+            }
+
+            if (matchingParameterExpressionId == 0)
+            {
+                return;
+            }
+
+            if (inputExpression == null)
+            {
+                var tag = new FPropertyTag() { 
+                    Name = Archive.GetOrCreateName("Expression"),
+                    Type = Archive.GetOrCreateName("ObjectProperty"),
+                    Size = 4,
+                    ArrayIndex = 0
+                };
+
+                inputExpression = new USerializedObjectProperty(Archive, /* backingProperty */ null, tag);
+                inputProp.TaggedProperties.Add(inputExpression);
+            }
+
+            inputExpression.ObjectIndex = matchingParameterExpressionId;
+        }
     }
 }
